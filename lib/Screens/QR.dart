f9ef05d9c39a62395b71f56679a555b0e5c1b9a3
bkmglasses <bkmglasses@gmail.com>
2025-02-02 21:12:19 +0200

    final update

--- lib/Screens/QR.dart
+++ lib/Screens/QR.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:qr_flutter/qr_flutter.dart';
 import 'package:senior/Screens/HomeScreen.dart';
-import '../Widget/Button.dart';
 
 
 class QRPage extends StatelessWidget {
